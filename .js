4a321513c9551afee79e96f3645155830deab7df
Unko <kobie.watkins@gmail.com>
2020-07-15 19:52:58 -0400

    initial code and updates to the portfolio page, creating dropdown buttons and links

---
+++ .js
@@ -0,0 +1,10 @@
+const myFunction = document.getElementsByClassName("myfunction");
+const portfolio = document.getElementsByClassName("portfolio");
+const codingQuiz = document.getElementsByClassName("C-Q");
+const passWordGenerator = document.getElementsByClassName("P-G");
+const workDayScheduler = document.getElementsByClassName("W-D-S");
+
+// when the user clicks on the button
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
