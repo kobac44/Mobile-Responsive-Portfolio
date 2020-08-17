a891032ccb7ea161497b176f0bc02df75baaaec9
Unko <kobie.watkins@gmail.com>
2020-08-16 20:18:36 -0400

    did not make many changes-2 possibly

---
+++ .js
@@ -0,0 +1,26 @@
+// const myFunction = document.getElementsByClassName("myfunction");
+// const portfolio = document.getElementsByClassName("portfolio");
+// const codingQuiz = document.getElementsByClassName("Coding-Quiz");
+// const passWordGenerator = document.getElementsByClassName("Password-Generator");
+// const workDayScheduler = document.getElementsByClassName("WorkDay-Scheduler");
+
+// // when the user clicks on the button
+// function myFunction() {
+//   document.getElementById("Coding-Quiz").classList.toggle("show");
+//   document.getElementById("Password-Generator").classList.toggle("show");
+//   document.getElementById("WorkDay-Scheduler").classList.toggle("show");
+// }
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function (event) {
+//   if (!event.target.matches(".dropbtn")) {
+//     const dropdowns = document.getElementsByClassName("-");
+//     const i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       const openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains("show")) {
+//         openDropdown.classList.remove("show");
+//       }
+//     }
+//   }
+// };
+// // console.log(dropdown, classList);
